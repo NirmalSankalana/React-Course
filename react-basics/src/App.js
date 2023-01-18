96fe64ecd79650e6d44cf8a252d71efff51f255a
Nirmal Sankalana <nirmal.s@eyepax.com>
2023-01-18 17:36:13 +0530

    UseState

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,35 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Card from "./Card";
 import faker from "faker";
+import person1 from "./img/person_1.jpg";
+
 function App() {
+  const [name, setName] = useState("Lily Collins");
   const buttonsMarkup = (
     <div>
       <button className="button button2">YES</button>
       <button className="button button3">NO</button>
     </div>
   );
+  const changeNameHandler = () => {
+    setName("Umaria");
+  };
   return (
     <div className="App">
-      <Card
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
-        title={faker.name.jobTitle()}
-      >
-        {buttonsMarkup}
-      </Card>
-      <Card
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
-        title={faker.name.jobTitle()}
-      >
-        {buttonsMarkup}
-      </Card>
-      <Card
-        avatar={faker.image.avatar()}
-        name={`${faker.name.firstName()} ${faker.name.firstName()}`}
-        title={faker.name.jobTitle()}
-      >
+      <button className="button" onClick={changeNameHandler}>
+        Change Name
+      </button>
+      <Card avatar={person1} name={name}>
         {buttonsMarkup}
       </Card>
     </div>
