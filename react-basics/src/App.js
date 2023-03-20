a2a7bd072e26213995fbfd3ba864c2d57b65904e
Nirmal Sankalana <sankalana.nirmal@gmail.com>
2023-03-21 02:23:32 +0530

    Convert to class based components

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,72 +1,81 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 import Card from "./Card";
 import { faker } from "@faker-js/faker";
+import styled from "styled-components";
 
-function App() {
-  const [cards, setCards] = useState([
-    {
-      _id: 1,
-      name: faker.name.fullName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar(),
-    },
-    {
-      _id: 2,
-      name: faker.name.fullName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar(),
-    },
-    {
-      _id: 3,
-      name: faker.name.fullName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar(),
-    },
-    {
-      _id: 4,
-      name: faker.name.fullName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar(),
-    },
-    {
-      _id: 5,
-      name: faker.name.fullName(),
-      title: faker.name.jobTitle(),
-      avatar: faker.image.avatar(),
-    },
-  ]);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {
+          _id: 1,
+          name: faker.name.fullName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+        {
+          _id: 2,
+          name: faker.name.fullName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+        {
+          _id: 3,
+          name: faker.name.fullName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+        {
+          _id: 4,
+          name: faker.name.fullName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+        {
+          _id: 5,
+          name: faker.name.fullName(),
+          title: faker.name.jobTitle(),
+          avatar: faker.image.avatar(),
+        },
+      ],
+      showCard: true,
+    };
+  }
 
-  const buttonStyle = {
-    backgroundColor: null,
-  };
+  render() {
+    const buttonStyle = {
+      backgroundColor: null,
+    };
 
-  if (cards.length < 3) buttonStyle.backgroundColor = "yellow";
-  if (cards.length < 2) buttonStyle.backgroundColor = "orange";
-  if (cards.length < 1) buttonStyle.backgroundColor = "red";
+    if (this.state.cards.length < 3) buttonStyle.backgroundColor = "yellow";
+    if (this.state.cards.length < 2) buttonStyle.backgroundColor = "orange";
+    if (this.state.cards.length < 1) buttonStyle.backgroundColor = "red";
 
-  const deleteCardHandler = (index) => {
-    const cards_copy = [...cards];
-    cards_copy.splice(index, 1);
-    setCards(cards_copy);
-  };
+    const deleteCardHandler = (index) => {
+      const cards_copy = [...this.state.cards];
+      cards_copy.splice(index, 1);
+      this.setState({ cards: cards_copy });
+    };
 
-  return (
-    <div className="App">
-      <button style={buttonStyle} className="button-blue">
-        Show Card
-      </button>
-      {cards.map((card, index) => (
-        <Card
-          key={card._id}
-          avatar={card.avatar}
-          name={card.name}
-          title="hehe"
-          onDelete={() => deleteCardHandler(index)}
-        />
-      ))}
-    </div>
-  );
+    return (
+      <div className="App">
+        <button style={buttonStyle} className="button-blue">
+          Show Card
+        </button>
+        {this.state.cards.map((card, index) => (
+          <Card
+            key={card._id}
+            avatar={card.avatar}
+            name={card.name}
+            title="hehe"
+            onDelete={() => deleteCardHandler(index)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
